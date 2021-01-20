46f9c93814c3d1cb7444e1a20b13c0d1e08a076b
Rick <rickoliwer@gmail.com>
2021-01-20 22:16:25 +0100

    css styling

--- home.php
+++ home.php
@@ -2,27 +2,30 @@
 require_once 'header.php';
 require_once 'functions.php';
 ?>
+<main class="main-container">
 
+<div class="home-contant">
     <div>
         <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">Welcome back, <?php echo $_SESSION['user']['username'] ?>!</h2>
     </div>
 
 
-    <?php foreach(array_reverse($postsResults) as $allPosts) : ?>
-<div class="post-card">
-    <h3>Posted by <?php echo $_SESSION['user']['username'] ?></h3>
-    <div class="post">
-        <div class="post-img">
-            <img src="images/<?= $allPosts['image']?>"/>
-        </div>
-        <div class="post-content">
-            <h2><?= $allPosts['title']?></h2>
-            <p><?= $allPosts['textarea'] ?></p>   
-        </div>
-    </div>   
+<?php foreach(array_reverse($postsResults) as $allPosts) : ?>
+    <div class="post-card">
+        <h3>Posted by <?php echo $_SESSION['user']['username'] ?></h3>
+        <div class="post">
+            <div class="post-img">
+                <img src="images/<?= $allPosts['image']?>"/>
+            </div>
+            <div class="post-content">
+                <h2><?= $allPosts['title']?></h2>
+                <p><?= $allPosts['textarea'] ?></p>   
+            </div>
+        </div>   
+    </div>
+<?php endforeach; ?>
 </div>
-    <?php endforeach; ?>
-
+</main>
 </body>
 
 </html>
--- profile.php
+++ profile.php
@@ -14,24 +14,26 @@ require 'header.php';
         <h2>Profile</h2>
 
         <p>Your email: <?php echo $_SESSION['user']['email'] ?></p>
-        <h2>Responsive Polaroid Images / Cards</h2>
-        <?php if(isset($_GET['user'])):?>
-        <div class="posts">
-        <?php foreach(array_reverse($postByUser) as $userPosts) : ?>
-        <div class="post-card">
-        <div class="polaroid">
-        <img src="images/<?= $userPosts->image?>" alt="" style="width:100%">
-        <div class="post-container">
-        <h3><?= $userPosts->title?></h3>
-        <p><?= $userPosts->textarea ?></p>
-        </div>
-        </div>
-        </div>
-        <?php endforeach ;?>
-        <?php endif ;?>
+
+<?php if(isset($_GET['user'])):?>
+<?php foreach(array_reverse($postByUser) as $userPosts) : ?>
+    <div class="post-card">
+        <h3>Posted by <?php echo $_SESSION['user']['username'] ?></h3>
+        <div class="post">
+            <div class="post-img">
+                <img src="images/<?= $userPosts->image?>">
+            </div>
+            <div class="post-content">
+                <h2><?= $userPosts->title?></h2>
+                <p><?= $userPosts->textarea ?></p>   
+            </div>
+        </div>   
+    </div>
+<?php endforeach; ?>
+<?php endif ;?>
     </div>
 
-        </div>
+        
         <aside class="side-bar">
             <ul>
                 <li><a href="settings.php">Settings</a></li>
